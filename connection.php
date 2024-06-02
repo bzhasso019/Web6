81c6f20b3e9549e7fa8fa55c500096ccb9f6c9ba
bzhasso019 <144123070+bzhasso019@users.noreply.github.com>
2024-06-02 23:49:47 +0300

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,3 +1,13 @@
-<?php $db = new PDO('mysql:host=localhost;dbname=u67411', 'u67411', '9275980');
-    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+<?php
+    $db = new PDO('mysql:host=localhost;dbname=u67411', 'u67411', '9275980',
+    [PDO::ATTR_PERSISTENT => true, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+    
+    function checkAdmin($login, $password){
+        global $db;
+        if(isset($login) && isset($password)){
+            $qu = $db->prepare("SELECT id FROM users WHERE role = 'admin' and login = ? and password = ?");
+            $qu->execute([$login, md5($password)]);
+            return $qu->rowCount();
+        }
+    }
 ?>
